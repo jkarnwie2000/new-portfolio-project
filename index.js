914c3c2e2719dba44743450c3f36f4ed55df9ecb
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-08-20 12:05:16 -0400

    Refine photo page layout

---
+++ index.js
@@ -0,0 +1,31 @@
+function contact(event) {
+    event.preventDefault();    
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";    
+    emailjs 
+    .sendForm(
+        'service_56y7j1g',
+        'template_87bdkhg',
+         event.target,
+        '476QBa57rXxhABYJa'        
+    ).then(() => {        
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";        
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at richjuliusgold2000@yahoo.com."
+        );
+    })    
+}
+
+let isModalOpen = false;
+function toggleModal() {      
+    if (isModalOpen) {
+    isModalOpen = false;  
+    return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;    
+    document.body.classList += " modal--open";
+}
